7cebbb015b2dff308fca90f4dd45b1c0869e8b6d
Lian Xun <xun1991@gmail.com>
2017-01-12 10:02:43 +0800

    add test cases for custom partials extension

---
+++ test/customExtension.js
@@ -0,0 +1,35 @@
+'use strict';
+var assert = require('assert');
+var hbs = require('..');
+var path = require('path');
+var H = require('./helpers');
+
+
+// MEANJS is using custom extension .server.view.html instead of .hbs 
+// https://github.com/meanjs/mean
+describe('custom extension for partials view', function() {
+  var dirname = path.join(__dirname, 'views/customExtension');
+  var render = hbs.create().express4({
+      extname: '.server.view.html',
+      partialsDir: dirname + '/partialsDir'
+  });
+
+  it('should allow rendering multiple partials with custom extension', function(done) {
+    function check(err, html) {
+      assert.ifError(err);
+      assert.equal(
+        '<html>' + 
+          '<subpartial>1</subpartial>' + 
+          '<partial>1</partial>' + 
+          '<subpartial>2</subpartial>' + 
+          '<partial>2</partial>' + 
+        '</html>', 
+      H.stripWs(html));
+      done();
+    }
+
+    var options = {cache: true, settings: {views: dirname }};
+    render(dirname + '/template.server.view.html', options, check);
+  });
+
+});
