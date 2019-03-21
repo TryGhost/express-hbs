c756b6d3dc25b2d2d6e399013f69243b8a1eb124
Fabien O'Carroll <fabien@allou.is>
2019-03-21 15:20:25 +0100

    Added tests for dynamic template options

---
+++ test/dynamicTemplateOptions.js
@@ -0,0 +1,67 @@
+'use strict';
+var assert = require('assert');
+var path = require('path');
+var hbs = require('..');
+var H = require('./helpers');
+
+describe('dynamic template options', function () {
+  var dirname = path.join(__dirname, 'dynamicTemplateOptions');
+
+  describe('express3', function () {
+
+    it('merges res.locals._templateOptions with the self._templateOptions', function (done) {
+      var instance = hbs.create();
+      var render = instance.express3({});
+      instance.updateTemplateOptions({
+          data: {
+            greeting: 'Hello,',
+            firstName: 'Freddy',
+            lastName: 'Krueger'
+          } 
+      });
+
+      var locals = H.createLocals('express3', dirname, {
+        _templateOptions: {
+          data: {
+            lastName: 'Mercury'
+          } 
+        } 
+      });
+
+      render(path.join(dirname, 'template.hbs'), locals, function (err, html) {
+        assert.ifError(err);
+        assert.strictEqual(H.stripWs(html), H.stripWs('Hello, Freddy Mercury')); 
+        done(err);
+      });
+    });
+  });
+
+  describe('express4', function () {
+
+    it('merges res.locals._templateOptions with the self._templateOptions', function (done) {
+      var instance = hbs.create();
+      var render = instance.express4({});
+      instance.updateTemplateOptions({
+          data: {
+            greeting: 'Hello,',
+            firstName: 'Freddy',
+            lastName: 'Krueger'
+          } 
+      });
+
+      var locals = H.createLocals('express4', dirname, {
+        _templateOptions: {
+          data: {
+            lastName: 'Mercury'
+          } 
+        } 
+      });
+
+      render(path.join(dirname, 'template.hbs'), locals, function (err, html) {
+        assert.ifError(err);
+        assert.strictEqual(H.stripWs(html), H.stripWs('Hello, Freddy Mercury')); 
+        done(err);
+      });
+    });
+  });
+});
